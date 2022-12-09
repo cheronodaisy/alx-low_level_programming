81fd5c70021def60e6cb0a9fd75728e77146aeb1
cheronodaisy <jepchumbadaisy96@gmail.com>
2022-12-09 12:56:53 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
-/*
- *
- *  * main -  print lowercase and uppercase a-zA-Z using putchar
- *
- *   * Description: 'prints A-Z and a-z'
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+/**
+ * main - print lowercase and uppercase a-zA-Z using putchar
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 
