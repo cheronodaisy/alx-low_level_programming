af8926dd254a2987147bb7c26013db50edba5e20
cheronodaisy <jepchumbadaisy96@gmail.com>
2022-12-19 03:58:05 -0800

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses string
+ * @s: string to be reversed
+ * Return: nothing
+*/
+
+void rev_string(char *s)
+
+{
+	int len, i;
+
+	char *begin_ptr, *end_ptr, ch;
+
+	len = strlen(s);
+	begin_ptr = s;
+	end_ptr = s;
+
+	for (i = 0; i < len - 1; i++)
+		end_ptr++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		ch = *end_ptr;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = ch;
+		begin_ptr++;
+		end_ptr--;
+	}
+}
+
+
