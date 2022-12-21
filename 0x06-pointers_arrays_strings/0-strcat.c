07a5b9e699ade877c4e1217705b15adc528c2a51
cheronodaisy <jepchumbadaisy96@gmail.com>
2022-12-21 07:58:02 -0800

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
 /**
- * void prototypes
- * int prototypes
+ * _strcat - concatenates two strings
+ * @dest: pointer
+ * @src: pointer
+ * Return: dest
 */
 
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
 
-#endif /*MAIN_H*/
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
