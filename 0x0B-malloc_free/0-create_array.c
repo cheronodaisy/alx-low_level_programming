01063bcb09107845f46b5fa293f8164f221739fe
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-01-11 05:05:31 -0800

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,18 +11,13 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int index;
+	char *str,
+	unsigned int i;
 
-	if (size <= 0)
-	{
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-	}						}
-	array = malloc(sizeof(char) * size);
-	if (array == NULL)
-		return (NULL);
-
-	for (index = 0; index < size; index++)
-	array[index] = c;
-	return (array);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
