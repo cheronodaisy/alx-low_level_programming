172dc27088232492673c7c2ce56b36ab6099c84f
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-01-17 05:38:24 -0800

    SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * 4-sum.h: Header file that defines a function-like macro SUM(x, y)
+ * @x: argument
+ * @y: argument
+*/
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
