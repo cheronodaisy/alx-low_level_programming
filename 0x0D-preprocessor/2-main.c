598ae755a5977fa74c20ce2d2d81ddb1472f9f24
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-01-17 05:22:46 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file from which the program was printed
+ * Return: Always 0.
+*/
+
+int main(void)
+
+{
+	printf("The name of the file: %s\n", __FILE__);
+	return (0);
+}
